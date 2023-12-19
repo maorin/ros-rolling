1ce1e949af5d44dff0297f724a5a6324a0b46054
maojingjing <supermaojj@qq.com>
2023-12-19 22:36:28 +0800

    完成adb命令序列

--- src/scrcpy_ros/scrcpy_ros/auto_aim.py
+++ src/scrcpy_ros/scrcpy_ros/auto_aim.py
@@ -5,6 +5,8 @@ from cv_bridge import CvBridge
 from lane_msg.msg import Detection
 import cv2
 import numpy as np
+from ppadb.client import Client as AdbClient
+import threading
 
 class AutoAim(Node):
     def __init__(self):
@@ -13,6 +15,13 @@ class AutoAim(Node):
         self.bridge = CvBridge()
         self.current_detections = []
         self.target_position = None
+        self.adb_commands = []
+        self.client = AdbClient(host="127.0.0.1", port=5037)
+        self.devices = self.client.devices()
+
+        # 初始化锁
+        self.lock = threading.Lock()
+        self.command_thread = None
 
         x, y, w, h = 100, 100, 50, 50  # 假设的初始边界框
         empty_image = np.zeros((480, 640, 3), dtype=np.uint8)
@@ -31,15 +40,57 @@ class AutoAim(Node):
             10
         )
 
+    def execute_adb_command(self, lock, device, command):
+        with lock:
+
+            print("runing  command:",command)
+            device.shell(f"input swipe {command[0]} {command[1]} {command[2]} {command[3]} {command[4]}")
+            # 命令执行完成后，锁会自动释放
+
+    def swipe(self, device, x1, y1, x2, y2, duration):
+        device.shell(f"input swipe {x1} {y1} {x2} {y2} {duration}")
+
+    def generate_adb_commands(self, start_x, start_y, displacement_x, displacement_y, steps):
+        commands = []
+        step_x = displacement_x // steps
+        step_y = displacement_y // steps
+        current_x, current_y = start_x, start_y
+
+        for _ in range(steps):
+            next_x = current_x + step_x
+            next_y = current_y + step_y
+            commands.append((current_x, current_y, next_x, next_y, 10))
+            current_x, current_y = next_x, next_y
+
+        return commands
+
+    def calculate_displacement(self, screen_center_x, screen_center_y, target_x, target_y):
+        displacement_x = target_x - screen_center_x
+        displacement_y = target_y - screen_center_y
+        return displacement_x, displacement_y
+
+    def is_point_inside_rectangle(self, left, top, right, bottom, center_x, center_y):
+        if left <= center_x <= right and top <= center_y <= bottom:
+            return True
+        else:
+            return False
+    
 
     def largest_target_detection_callback(self, msg):
         self.current_detections = [msg]
         self.get_logger().info('Received largest_target: %s (%f)' % (msg.class_name, msg.confidence))
 
+        if self.command_thread:
+            self.command_thread.join()
 
+        for command in self.adb_commands:
+            if self.devices:
+                device = self.devices[0]
 
-        #在这里添加自瞄代码
-
+                self.command_thread = threading.Thread(
+                    target=self.execute_adb_command, 
+                    args=(self.lock, device, command))
+                self.command_thread.start()
 
 
 
@@ -77,15 +128,43 @@ class AutoAim(Node):
             bottom = int(y + h / 2)
             center_x = x + w / 2
             center_y = y + h / 2
+
             print("center_x:",center_x)
             print("center_y:",center_y)
 
+            # 判断准心在不在目标内
+            if self.is_point_inside_rectangle(left, top, right, bottom, center_x, center_y):
+                self.get_logger().info('Target is inside the screen')
+                
+                return
+            
+
+            # 目标在屏幕中的坐标
             target_x = center_x * 2340 / 800
             target_y = center_y * 1080 / 416
 
             print("target_x:",target_x)
             print("target_y:",target_y)
-            # 在这里添加自瞄代码
+
+            # 屏幕中心点
+            screen_center_x = 2340 / 2
+            screen_center_y = 1080 / 2
+
+            print("screen_center_x:",screen_center_x)
+            print("screen_center_y:",screen_center_y)
+
+            # 按住屏幕的初始坐标
+            hold_init_x =  2340 / 2 + 500
+            hold_init_y =  1080 / 2
+            
+            print("hold_init_x:",hold_init_x)
+            print("hold_init_y:",hold_init_y)
+
+            # 生成滑动命令
+            displacement_x, displacement_y = self.calculate_displacement(screen_center_x, screen_center_y, target_x, target_y)
+
+            self.adb_commands = self.generate_adb_commands(screen_center_x, screen_center_y, displacement_x, displacement_y, steps=10)
+
 
 
             cv2.rectangle(cv_image_resized, (left, top), (right, bottom), (0, 255, 0), 2)
